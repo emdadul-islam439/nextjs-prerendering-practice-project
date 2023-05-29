8db307f974f5adb9f03e670e495d42963868418a
Emdadul_Islam <emdadulislam439@gmail.com>
2023-05-29 23:21:07 +0600

    started ch-05 vid-16--> make fallback:true and ommitted some paths of pre-rendering. Now, the p1 will be pre-rendered but p2 and p3 will be rendered just-in-time

--- pages/[pid].js
+++ pages/[pid].js
@@ -35,10 +35,8 @@ export async function getStaticPaths() {
   return {
     paths: [
       { params: { pid: "p1" } },
-      { params: { pid: "p2" } },
-      { params: { pid: "p3" } },
     ],
-    fallback: false,
+    fallback: true,
   };
 }
 
