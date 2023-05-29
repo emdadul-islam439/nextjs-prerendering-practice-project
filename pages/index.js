94c404ab20fa6e6806039498ec411763ad498d26
Emdadul_Islam <emdadulislam439@gmail.com>
2023-05-29 18:16:16 +0600

    finished ch-05 vid-06--> used 'getStaticProps()' async-function to render data in build time (not run time)

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
-function HomePage() {
-  return <ul>
-    <li>Product 1</li>
-    <li>Product 2</li>
-    <li>Product 1</li>
-  </ul>;
+function HomePage(props) {
+  const { products } = props;
+
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [{ id: "p1", title: "Product 1" }],
+    },
+  };
 }
 
 export default HomePage;
